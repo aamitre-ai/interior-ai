fd6906fb4dcdaa827dbc70161267e761ea6424c0
aamitre-ai <aalvarez845@gmail.com>
2026-06-12 10:28:20 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "replicate.delivery" },
-      { protocol: "https", hostname: "pbxt.replicate.delivery" },
-    ],
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
 };
-
 export default nextConfig;
